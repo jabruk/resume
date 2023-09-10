fac138b3998b2e0960f7f75c1dab13c7c3c059ff
Your Name <you@example.com>
2023-09-10 16:57:40 +0000

    Footer with svg

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,14 +12,16 @@
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     </head>
-    <body class="antialiased">
+    <body class="antialiased text-gray-800 dark:text-gray-200">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900 pt-24">
         <!-- ====== Navbar Section Start -->
         <x-layout.navbar></x-layout.navbar>
         <!-- ====== Navbar Section End -->
 
             {{ $slot }}
 
-        <footer>Footer</footer>
+            <x-layout.footer></x-layout.footer>
 
+        </div>
     </body>
 </html>
